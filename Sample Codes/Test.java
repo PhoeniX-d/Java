285d37c5022f6a9fa8b5c63b75d49d84144ff5d0
PhoeniX-d <prnv24choudhary@gmail.com>
2020-12-15 15:28:27 +0530

    Sample Codes

--- Sample Codes/Test.java
+++ Sample Codes/Test.java
@@ -2,7 +2,24 @@ public class Test
 {
     public static void main(String args[])   
     {
-        int i = 0xBeer;
-        System.out.println(i);
+        int[] i = new int[3];
+        float[] f = new float[2];
+        char[] ch = new char[1];
+        long[] l = new long[6];
+        byte[] b = new byte[2];
+        boolean[] z = new boolean[4];
+        double[] d = new double[2];
+        short[] s = new short[2];
+        String str[] = new String[2];
+
+        System.out.println("int\t:" + i.getClass().getName());
+        System.out.println("float\t:" + f.getClass().getName());
+        System.out.println("long\t:" + l.getClass().getName());
+        System.out.println("char\t:" + ch.getClass().getName());
+        System.out.println("byte\t:" + b.getClass().getName());
+        System.out.println("boolean\t:" + z.getClass().getName());
+        System.out.println("double\t:" + d.getClass().getName());
+        System.out.println("short\t:" + s.getClass().getName());
+        System.out.println("String\t:" + str.getClass().getName());
     }
 }
