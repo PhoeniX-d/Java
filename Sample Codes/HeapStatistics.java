11cced498929fd08949cf565d97838d78b1450db
PhoeniX-d <prnv24choudhary@gmail.com>
2020-12-22 22:07:07 +0530

    Java Heap Statistics demo

---
+++ Sample Codes/HeapStatistics.java
@@ -0,0 +1,12 @@
+class HeapStatistics
+{
+	public static void main(String[] args)
+	{
+		double mb = 1024 * 1024;
+		Runtime r = Runtime.getRuntime();
+		System.out.println("Max Memory:" + r.maxMemory() / mb);
+		System.out.println("Initial Memory:" + r.totalMemory() / mb);
+		System.out.println("Free Memory:" + r.freeMemory() / mb);
+		System.out.println("Consumed Memory:"  + (r.totalMemory() - r.freeMemory()) / mb);
+	}
+}
