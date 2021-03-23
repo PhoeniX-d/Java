fc9422e6cf446c0ab970b9f75f9e68a8ff0439aa
PhoeniX-d <prnv24choudhary@gmail.com>
2021-03-23 22:29:45 +0530

    Files Related

---
+++ FileIO/DuplicateDataRemove.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+class DuplicateDataRemove
+{
+    public static void main(String[] args) throws IOException
+    {
+        PrintWriter pw = new PrintWriter("file3.txt");
+        BufferedReader br1 = new BufferedReader(new FileReader("file1.txt"));
+        String line = br1.readLine();        
+        while (line != null)
+        {
+            BufferedReader br2 = new BufferedReader(new FileReader("file3.txt"));
+            boolean isAvail = false;
+            String target = br2.readLine();
+            while(target != null)
+            {
+                if (line.equals(target)) {
+                    isAvail = true;
+                    break;
+                }
+                target = br2.readLine();
+            }
+            if(isAvail == false)
+            {
+                pw.println(line);
+                pw.flush();
+            }
+            line = br1.readLine();
+            br2.close();
+        }
+        br1.close();
+        pw.close();
+    }    
+}
