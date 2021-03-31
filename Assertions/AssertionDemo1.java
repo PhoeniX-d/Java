43cdbfd8a99b2ee3cc17ff1b9a979eaac2c133a0
PhoeniX-d <prnv24choudhary@gmail.com>
2021-03-31 10:02:40 +0530

    Assert

---
+++ Assertions/AssertionDemo1.java
@@ -0,0 +1,15 @@
+class AssertDemo1
+{
+    public static void main(String[] args) 
+    {
+        int x = 10;
+        ;
+        ;
+        ;
+        assert (x > 10);
+        ;
+        ;
+        ;
+        System.out.println(x); 
+    }
+}
---
+++ Assertions/AugmentAssertDemo.java
@@ -0,0 +1,15 @@
+class AugmentAssertDemo
+{
+    public static void main(String[] args) 
+    {
+        int x = 10;
+        ;
+        ;
+        ;
+        assert (x > 10) : "Here value of x should be > 10";
+        ;
+        ;
+        ;
+        System.out.println(x); 
+    }
+}
