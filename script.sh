e666561ee5deb7122934fa554d6fa5059f3d2c1d
PhoeniX-d <prnv24choudhary@gmail.com>
2021-05-25 13:00:52 +0530

    Problems

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
-git pull https://github.com/PhoeniX-d/Java
+git pull https://PhoeniX-d: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@github.com/PhoeniX-d/Java
 git add -A
-git commit -m "Problems On Numbers"
+git commit -m $1
 git push origin master
 
