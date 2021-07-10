c9a953e213eb5e3f712fa4d53ee667a01d7c77df
PhoeniX-d <prnv24choudhary@gmail.com>
2021-07-10 18:24:32 +0530

    FirstServletApp

---
+++ ServletAndJSP/ServletFirstDemo/src/FirstServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+// Without using Deployement description use annotationss
+//@WebServlet("/welcome")
+public class FirstServlet extends GenericServlet
+{
+    public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException
+    {
+        PrintWriter out = resp.getWriter();
+        out.println("<h1> Welcom to Servlet");
+    }
+}
